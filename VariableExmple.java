b55e49a63ea7985a2ab655001717d4c4336b003b
684259026-crypto <>
2025-07-25 11:41:13 +0700

    First commit

---
+++ VariableExmple.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class A {
+  public static void main(String args[]) {
+     char clothingSize='s';
+     int websiteVisitors = 0;
+     char studentGrade = 'A';
+     double studentGPA = 4.0;
+     final double PI = 3.14159;
+     float annualExpenses = 15000.00f;
+     final int MoNTHS_IN_YEAR =12;
+     System.out.println("ClothingSiz")
+     System.out.println("WebsiteVisi")
+     System.out.println("StudentGrad")
+     System.out.println("StudentGPA")
+     System.out.println("PI")
+     System.out.println("AnnualExpenses")
+     System.out.println("MoNTHS_IN_YEAR")
+  }  
+}
